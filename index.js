2fd7f9c5fa69ed0a9445a85687de19f369f5547b
wgyhusijk <gs1skylever@hotmail.com>
2024-05-11 05:12:02 +0700

    test project

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
+const dropRight = (arr, n = 1) => arr.slice(0, -n);
