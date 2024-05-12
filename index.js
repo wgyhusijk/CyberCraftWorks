d05133db50104cdbd2428bcce31c88058ad7976e
wgyhusijk <gs1skylever@hotmail.com>
2024-05-13 02:41:41 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
-  }
-  return -1;
-}
+const initializeArrayWithRange = (end, start = 0) =>
+  Array.from({ length: end - start }, (_, i) => i + start);
