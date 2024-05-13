15555600ed51ced921ed2b7a3fc85f4287afff89
wgyhusijk <gs1skylever@hotmail.com>
2024-05-14 01:41:14 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const initializeArrayWithRange = (end, start = 0) =>
-  Array.from({ length: end - start }, (_, i) => i + start);
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
