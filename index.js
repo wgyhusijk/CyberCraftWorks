21de3e4890a9af8f81003b2a62bc33397c749d9d
wgyhusijk <gs1skylever@hotmail.com>
2024-05-16 22:46:41 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
-function trapRainWater(heightMap) {
-  if (heightMap.length === 0) return 0;
-  const rows = heightMap.length;
-  const cols = heightMap[0].length;
-  const visited = Array.from(Array(rows), () => Array(cols).fill(false));
-  const heap = new MinHeap();
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (i === 0 || i === rows - 1 || j === 0 || j === cols - 1) {
-        heap.insert([heightMap[i][j], i, j]);
-        visited[i][j] = true;
-      }
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
   }
-  const dirs = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0],
-  ];
-  let waterTrapped = 0;
-  while (heap.size() > 0) {
-    const [height, row, col] = heap.pop();
-    for (const dir of dirs) {
-      const newRow = row + dir[0];
-      const newCol = col + dir[1];
-      if (
-        newRow >= 0 &&
-        newRow < rows &&
-        newCol >= 0 &&
-        newCol < cols &&
-        !visited[newRow][newCol]
-      ) {
-        visited[newRow][newCol] = true;
-        waterTrapped += Math.max(0, height - heightMap[newRow][newCol]);
-        heap.insert([
-          Math.max(height, heightMap[newRow][newCol]),
-          newRow,
-          newCol,
-        ]);
-      }
-    }
-  }
-  return waterTrapped;
+  return dp[m - 1][n - 1];
 }
