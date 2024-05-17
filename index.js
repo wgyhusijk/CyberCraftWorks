4697e6f5ff3445229c7faef88ed7a3703c1f4786
wgyhusijk <gs1skylever@hotmail.com>
2024-05-18 05:23:11 +0700

    feat

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function uniquePaths(m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const map = new Map();
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
   }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
+  for (const char of t) {
+    if (!map.has(char) || map.get(char) === 0) return false;
+    map.set(char, map.get(char) - 1);
   }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+  return true;
 }
